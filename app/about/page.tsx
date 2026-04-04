8e91def28498ff5051559b77b7480037ea0720b4
Farhan Hajid <farhan.hajid101@gmail.com>
2026-04-04 23:14:40 +0700

    updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,18 @@ import Header from '../component/header'
 
 export default function aboutPage() {
   return (
-    <main>
-      <Header />
+    <>
     
+    <Header />
+
+    <main style={{ fontFamily: 'sans-serif', maxWidth: '720px', margin: '0 auto', padding: '2.5rem 1.5rem' }}>
+            
+
       <h1>About Page</h1>
       <p>Welcome to the About Page!</p>
     </main>
+    
+    
+    </>
   )
 }
