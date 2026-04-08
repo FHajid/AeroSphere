167f1a0b8ac0d5c90a31d08d13d498a4574de207
Farhan Hajid <farhan.hajid101@gmail.com>
2026-04-08 21:56:10 +0700

    the about framing is done now is only for the CSS/tailwind to make it look better.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,11 +8,12 @@ import Nakedbike from '../../public/Nakedbike.jpg'
 
 export default function aboutPage() {
   return (
-    <>
+    <div className="bg-white min-h-screen text-black">
     
     <Header />
 
-    <main style={{ fontFamily: 'sans-serif', maxWidth: '1020px', margin: '0 auto', padding: '1.5rem ' }}>
+    <main 
+    style={{ fontFamily: 'sans-serif', maxWidth: '1020px', margin: '0 auto', padding: '1.5rem ' }}>
             
       <section>
         <h1 className='text-6xl'>ABOUT US</h1>
@@ -47,10 +48,42 @@ export default function aboutPage() {
 
       </section>
 
+      <section className="flex items-center">
+        <div className="bg-gray-200 p-4 rounded-lg">
+          <h2 className='text-3xl m-4'>Our Team</h2>
+          <p className='text-lg m-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+        <div className="bg-gray-200 p-4 rounded-lg ml-4">
+          <h2 className='text-3xl m-4'>Our Values</h2>
+          <p className='text-lg m-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+        <div className="bg-gray-200 p-4 rounded-lg ml-4">
+          <h2 className='text-3xl m-4'>Our Vision</h2>
+          <p className='text-lg m-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+
+      </section>
+
+    <section className='item-center text-center p-4 bg-gray-100'>
       
+      <p className='text-4xl m-4'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <h1 className='text-7xl'>Contact US</h1>
+    </section>
+
     </main>
     
     
-    </>
+
+    </div>
   )
 }
