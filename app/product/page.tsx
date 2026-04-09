4102930bd6090018720e1d7a56b591d0e71973fd
Farhan Hajid <farhan.hajid101@gmail.com>
2026-04-09 22:00:57 +0700

    The product page  wireframe have been place (Need to add more to it) and a Design not yet

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import Header from '../component/header'
+import Footer from '../component/footer'
 
 export default function productPage() {
   return (
-    <main>
+    <div>
         <Header />
+        <main>
+        {/* PRODUCT CAROUSEL SECTION */}
+        <section>
         <h1>Product Page</h1>
         <p>Welcome to the Product Page!</p>
-    </main>
+        </section>
+
+      {/* PRODUCTS CATEGORY SECTION */}
+        <section>
+        <h2>Our Products</h2>
+        </section>
+      {/* FEATURED PRODUCTS SECTION */}
+        <section>
+
+        </section>
+
+        <article>
+
+        </article>
+
+        </main>   
+        <Footer />
+        
+    </div>
   )
 }
