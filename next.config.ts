04e3cb938fcde9d8073db3f6cc89dcf0a31f14b4
Farhan Hajid <farhan.hajid101@gmail.com>
2026-05-11 22:07:06 +0700

    Fix: force webpack build flag

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  compress: true,
-  // Kita hapus semua blok 'turbo' yang tadi bikin error
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glb|gltf|bin)$/,
